f947182174d3e9d16d62a9b360302f12fc387af1
Tommi Ahonen <tomaho1129@skola.goteborg.se>
2021-05-21 16:38:24 +0200

    Create ParkingService interface.

---
+++ src/se/yrgo/service/ParkingService.java
@@ -0,0 +1,14 @@
+package se.yrgo.service;
+
+import javax.ejb.Local;
+
+import se.yrgo.domain.Employee;
+import se.yrgo.domain.ParkingTicket;
+
+@Local
+public interface ParkingService {
+
+	public void createTicket(ParkingTicket ticket);
+	public void deleteTicket(int ticketId);
+	public void findTicketById(int ticketId);
+}
