b0a32c90d1bae0aca2788bc81a010aa769b76e48
Tommi Ahonen <tomaho1129@skola.goteborg.se>
2021-05-21 17:01:02 +0200

    Create empty implementation of parking service.

---
+++ src/se/yrgo/service/ParkingServiceImplementation.java
@@ -0,0 +1,25 @@
+package se.yrgo.service;
+
+import se.yrgo.domain.ParkingTicket;
+
+public class ParkingServiceImplementation implements ParkingService {
+
+	@Override
+	public void createTicket(ParkingTicket ticket) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void deleteTicket(int ticketId) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void findTicketById(int ticketId) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
