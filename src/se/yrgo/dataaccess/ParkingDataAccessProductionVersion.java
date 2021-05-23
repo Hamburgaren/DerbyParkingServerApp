3d135b0dcdb9223b04f3fc098b90311e5307aa82
Tommi Ahonen <tomaho1129@skola.goteborg.se>
2021-05-23 15:41:29 +0200

    Create some storeage-error exceptions.

--- src/se/yrgo/dataaccess/ParkingDataAccessProductionVersion.java
+++ src/se/yrgo/dataaccess/ParkingDataAccessProductionVersion.java
@@ -36,27 +36,8 @@ public class ParkingDataAccessProductionVersion implements ParkingDataAccess {
 	}
 
 	@Override
-<<<<<<< HEAD
-	public void createTicket(ParkingTicket ticket) throws ParkingTicketAlreadyExistsException, StorageException {
-		
-		System.out.printf("Now creating new ParkingTicket %s in database.", ticket);
-		//EntityTransaction tx = em.getTransaction();
-		//tx.begin();
-		try {
-			//Sem.persist(ticket);
-		} catch (EntityExistsException ex) {
-			throw new ParkingTicketAlreadyExistsException(ex);
-		} catch (Exception ex) {
-			throw new StorageException("Error: Unable to store new ticket in database", ex);
-		}
-		// TODO: Remove once database query has been implemented
-		throw new StorageException("Error: Unable to store new ticket in database");
-		//tx.commit();
-		//em.close();
-=======
 	public void createTicket(ParkingTicket ticket) {
 		em.persist(ticket);
->>>>>>> 45c0515301153c6a1127391dbcd78139458c576e
 		
 	}
 
