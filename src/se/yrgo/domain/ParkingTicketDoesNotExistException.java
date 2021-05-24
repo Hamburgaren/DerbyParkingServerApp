76400b980f153046752b8761ec270503b28c54b1
Tommi Ahonen <tomaho1129@skola.goteborg.se>
2021-05-24 20:16:44 +0200

    Add HTTP PUT method to REST layer.

---
+++ src/se/yrgo/domain/ParkingTicketDoesNotExistException.java
@@ -0,0 +1,30 @@
+package se.yrgo.domain;
+
+public class ParkingTicketDoesNotExistException extends Exception {
+
+	public ParkingTicketDoesNotExistException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public ParkingTicketDoesNotExistException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ParkingTicketDoesNotExistException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ParkingTicketDoesNotExistException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ParkingTicketDoesNotExistException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+}
