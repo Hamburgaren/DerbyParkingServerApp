ead9bfd9dea70ff3b799bcfca434e29ff1b291e5
Mehrdadbadeie <72803122+Mehrdadbadeie@users.noreply.github.com>
2021-05-26 10:14:30 +0200

    Update ParkingServiceLocal.java

--- src/se/yrgo/service/ParkingServiceLocal.java
+++ src/se/yrgo/service/ParkingServiceLocal.java
@@ -5,6 +5,7 @@ import javax.ejb.Remote;
 
 import se.yrgo.domain.ParkingTicket;
 import se.yrgo.exceptions.ParkingTicketAlreadyExistsException;
+import se.yrgo.exceptions.ParkingTicketDoesNotExistException;
 import se.yrgo.exceptions.StorageAccessException;
 
 @Remote
@@ -13,5 +14,5 @@ public interface ParkingServiceLocal {
 	public void createTicket(ParkingTicket ticket) throws ParkingTicketAlreadyExistsException, StorageAccessException;
 	public boolean deleteTicket(int ticketId) throws StorageAccessException;
 	public ParkingTicket findTicketById(int ticketId) throws StorageAccessException;
-	
+	public void updateTicket(ParkingTicket newTicket) throws StorageAccessException, ParkingTicketDoesNotExistException;
 }
